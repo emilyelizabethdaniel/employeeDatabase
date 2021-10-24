6ecc8f1530f5f039c93cb8fb07873b40dfa0217c
Basquiat <ememdaniel98@gmail.com>
2021-10-23 20:14:25 -0500

    added sql files, added on to server.js

--- server.js
+++ server.js
@@ -10,3 +10,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+
+const db = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'rootuser',
+        database: 'employees_db'
+    },
+    console.log("database connected!")
+);
